ce6c40b3e227de486529c4697a6a1a9619bdf6b7
basalbr <basal_@hotmail.com>
2016-10-13 16:44:13 -0300

    correção editar noticias

--- app/Http/Controllers/NoticiaController.php
+++ app/Http/Controllers/NoticiaController.php
@@ -13,9 +13,9 @@ class NoticiaController extends Controller {
         $noticias = Noticia::orderBy('created_at', 'desc')->get();
         return view('admin.noticias.index', ['noticias' => $noticias]);
     }
-    
-     public function indexSite() {
-        $noticias = Noticia::orderBy('created_at', 'desc')->where('created_at','<','NOW()')->get();
+
+    public function indexSite() {
+        $noticias = Noticia::orderBy('created_at', 'desc')->where('created_at', '<', 'NOW()')->get();
         return view('noticias.index', ['noticias' => $noticias]);
     }
 
@@ -23,7 +23,7 @@ class NoticiaController extends Controller {
         $noticia = Noticia::find($id);
         return view('noticias.ler', ['noticia' => $noticia]);
     }
-    
+
     public function create() {
         return view('admin.noticias.cadastrar');
     }
@@ -55,14 +55,27 @@ class NoticiaController extends Controller {
         return view('admin.noticias.editar', ['noticia' => $noticia]);
     }
 
-    public function update($id_imposto, $id_noticia, Request $request) {
+    public function update($id_noticia, Request $request) {
         $noticia = Noticia::where('id', '=', $id_noticia)->first();
-        $dados = $request->only('ordem', 'descricao');
-        if ($noticia->validate($dados)) {
-            $noticia = $noticia->update($dados);
-            return redirect(route('listar-noticia', ['id_imposto' => $id_imposto]));
+        if(!$request->file('imagem')){
+            $request->merge(['imagem'=>$noticia->imagem]);
+        }
+        if ($noticia->validate($request->all())) {
+            list($day, $month, $year) = explode("/", $request->created_at);
+            $request->merge(['created_at' => $year . '-' . $month . '-' . $day]);
+            if ($request->file('imagem')) {
+                $imagem = date('dmyhis') . '.' . $request->file('imagem')->guessClientExtension();
+                $request->file('imagem')->move(getcwd() . '/uploads/noticias/', $imagem);
+                $request->merge(['imagem' => $imagem]);
+                $img = Image::make(getcwd() . '/uploads/noticias/' . $imagem);
+                $img->fit(600, 400);
+                $img->save(getcwd() . '/uploads/noticias/thumb/' . $imagem);
+                $noticia->imagem = $imagem;
+            }
+            $noticia = $noticia->update($request->except('imagem'));
+            return redirect(route('listar-noticias'));
         } else {
-            return redirect(route('editar-noticia', ['id_imposto' => $id_imposto, 'id_noticia' => $id_noticia]))->withInput()->withErrors($noticia->errors());
+            return redirect(route('editar-noticia', ['id_noticia' => $id_noticia]))->withInput()->withErrors($noticia->errors());
         }
     }
 
--- resources/views/admin/noticias/editar.blade.php
+++ resources/views/admin/noticias/editar.blade.php
@@ -1,66 +1,73 @@
-@extends('layouts.master')
+@extends('layouts.admin')
 @section('header_title', 'Home')
 @section('js')
 @parent
 <script type="text/javascript" src="{{url('public/ckeditor/ckeditor.js')}}"></script>
 <script type="text/javascript" src="{{url('public/ckfinder/ckfinder.js')}}"></script>
+<script type="text/javascript" src="{{url('public/js/bootstrap-datepicker.min.js')}}"></script>
+<script type="text/javascript" src="{{url('public/js/bootstrap-datepicker.pt-BR.min.js')}}"></script>
 <script type="text/javascript" language="javascript">
 $(document).ready(function () {
+    $('.date-mask').on('keypress', function () {
+        return false;
+    });
+    $('.date-mask').datepicker({
+        language: 'pt-BR',
+        autoclose: true,
+        format: 'dd/mm/yyyy',
+        todayBtn: 'linked'
+    });
     //CKFinder.setupCKEditor();
-    CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	 config.filebrowserBrowseUrl = '{{url("public/kcfinder/browse.php?opener=ckeditor&type=files")}}';
-   config.filebrowserImageBrowseUrl =  '{{url("public/kcfinder/browse.php?opener=ckeditor&type=images")}}';
-   config.filebrowserFlashBrowseUrl = '{{url("public/kcfinder/browse.php?opener=ckeditor&type=flash")}}';
-   config.filebrowserUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=files")}}';
-   config.filebrowserImageUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=images")}}';
-   config.filebrowserFlashUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=flash")}}';
-   config.contentsCss = '{{url("public/css/custom.css")}}'
-};
+    CKEDITOR.editorConfig = function (config) {
+        // Define changes to default configuration here. For example:
+        // config.language = 'fr';
+        // config.uiColor = '#AADC6E';
+        config.filebrowserBrowseUrl = '{{url("public/kcfinder/browse.php?opener=ckeditor&type=files")}}';
+        config.filebrowserImageBrowseUrl = '{{url("public/kcfinder/browse.php?opener=ckeditor&type=images")}}';
+        config.filebrowserFlashBrowseUrl = '{{url("public/kcfinder/browse.php?opener=ckeditor&type=flash")}}';
+        config.filebrowserUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=files")}}';
+        config.filebrowserImageUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=images")}}';
+        config.filebrowserFlashUploadUrl = '{{url("public/kcfinder/upload.php?opener=ckeditor&type=flash")}}';
+        config.contentsCss = '{{url("public/css/custom.css")}}'
+    };
     CKEDITOR.replace('ckeditor');
 });
 </script>
 @stop
-@section('content')
-<section id='page-header' style="margin-top: 55px" class="page-header">
-    <div class='container'>
-        <h1>Editar Instrução</h1>
-    </div>
-</section>
-<section>
-    <div class="container">
+@section('main')
+<h1>Cadastrar Notícia</h1>
+<hr class="dash-title">
 
-        @if($errors->has())
-        <div class="alert alert-warning shake">
-            <b>Atenção</b><br />
-            @foreach ($errors->all() as $error)
-            {{ $error }}<br />
-            @endforeach
-        </div>
-        @endif
-        <form method="POST" action=""  enctype="multipart/form-data">
-            {{ csrf_field() }}
-            <div class='form-group'>
-                <label>Título</label>
-                <input type='text' class='form-control' name='titulo' value="{{$noticia->titulo}}"/>
-            </div>
-            <div class='form-group'>
-                <label>Texto</label>
-                <textarea id='ckeditor' class='form-control' name='texto'>{{$noticia->texto}}</textarea>
-            </div>
-            <div class='form-group'>
-                <label>Imagem principal</label>
-                <div class='input-group col-md-12'>
-                    <input type='file' class='form-control' value="" name='imagem'/>
-                </div>
-            </div>
-            <div class='form-group'>
-                <input type='submit' value="Cadastrar" class='btn btn-primary' />
-            </div>
-        </form>
-    </div>
+@if($errors->has())
+<div class="alert alert-warning shake">
+    <b>Atenção</b><br />
+    @foreach ($errors->all() as $error)
+    {{ $error }}<br />
+    @endforeach
 </div>
-</section>
+@endif
+<form method="POST" action=""  enctype="multipart/form-data">
+    {{ csrf_field() }}
+    <div class='form-group'>
+        <label>Título</label>
+        <input type='text' class='form-control' name='titulo' value="{{$noticia->titulo}}"/>
+    </div>
+    <div class='form-group'>
+        <label>Texto</label>
+        <textarea id='ckeditor' class='form-control' name='texto'>{{$noticia->texto}}</textarea>
+    </div>
+    <div class='form-group'>
+        <label>Imagem (Deixe em branco para não alterar)</label>
+        <div class='input-group col-md-12'>
+            <input type='file' class='form-control' value="" name='imagem'/>
+        </div>
+    </div>
+    <div class="form-group">
+        <label>Data de Publicação</label>
+        <input type="text" class="form-control date-mask" name='created_at' value='{{$noticia->created_at->format('d/m/Y')}}'/>
+    </div>
+    <div class='form-group'>
+        <input type='submit' value="Alterar Notícia" class='btn btn-primary' />
+    </div>
+</form>
 @stop
